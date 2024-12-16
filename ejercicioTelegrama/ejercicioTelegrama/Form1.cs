0768861529f795f5900c74953837445d38c735c9
Vicente379 <vicentegarciafernandez43@gmail.com>
2024-12-16 16:51:48 +0100

    Primer commit con código

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace ejercicioTelegrama
+namespace Telegrama
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCalcularPrecio_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama = ' ';
+            int numPalabras = 0;
+            double coste;
+
+            // Leo el telegrama
+            textoTelegrama = txtTelegrama.Text;
+
+            // Determino el tipo de telegrama
+            // Si el checkbox de urgente está marcado, se asigna 'u' a tipoTelegrama
+            if (chkUrgente.Checked)
+            {
+                tipoTelegrama = 'u';
+            }
+            else
+            {
+                // Solo asigna 'o' si no está urgente
+                tipoTelegrama = 'o';
+            }
+
+            // Obtengo el número de palabras que forma el telegrama
+            String[] palabras = textoTelegrama.Split(' '); //separa las palabras y las introduce en una estructura numPalabras = palabras.Length; //Obtiene la longitud de la estructura, que será el número de palabras.
+            numPalabras = palabras.Length;
+
+            // Si el telegrama es ordinario
+            if (tipoTelegrama == 'o')
+            {
+                if (numPalabras <= 10)
+                {
+                    coste = 2.5;
+                }
+                else
+                {
+                    // Se suma 2.5 por las primeras 10 palabras, y 0.5 por cada palabra adicional
+                    coste = 2.5 + 0.5 * (numPalabras - 10);
+                }
+            }
+            else if (tipoTelegrama == 'u')
+            {
+                // Telegrama urgente
+                if (numPalabras > 10)
+                {
+                    coste = 5 + 0.75 * (numPalabras - 10);
+                }
+                else
+                {
+                    // Se suma 5 por las primeras 10 palabras, y 0.75 por cada palabra adicional
+                    coste = 5;
+                }
+            }
+            else
+            {
+                coste = 0; // Esto se ejecuta si tipoTelegrama no es 'o' ni 'u'
+            }
+
+            // Formateo del precio para mostrarlo con dos decimales
+            txtPrecio.Text = coste.ToString("F2") + " euros";
+        }
+
+        private void chkUrgente_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- ejercicioTelegrama/ejercicioTelegrama/Program.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Program.cs
@@ -16,7 +16,7 @@ namespace ejercicioTelegrama
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Telegrama.Form1());
         }
     }
 }
