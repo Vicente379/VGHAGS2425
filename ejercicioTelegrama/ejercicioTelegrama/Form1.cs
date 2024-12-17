8e420b01097e31c1601398404d4a69e32f03e993
Hectorambl <hectorambit04@gmail.com>
2024-12-17 17:01:44 +0100

    Nueva Rama con los errores corregidos.

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -25,26 +25,25 @@ namespace Telegrama
             int numPalabras = 0;
             double coste;
 
-            // Leo el telegrama
+         
             textoTelegrama = txtTelegrama.Text;
 
-            // Determino el tipo de telegrama
-            // Si el checkbox de urgente está marcado, se asigna 'u' a tipoTelegrama
+            
             if (chkUrgente.Checked)
             {
                 tipoTelegrama = 'u';
             }
             else
             {
-                // Solo asigna 'o' si no está urgente
+                
                 tipoTelegrama = 'o';
             }
 
-            // Obtengo el número de palabras que forma el telegrama
-            String[] palabras = textoTelegrama.Split(' '); //separa las palabras y las introduce en una estructura numPalabras = palabras.Length; //Obtiene la longitud de la estructura, que será el número de palabras.
+            
+            String[] palabras = textoTelegrama.Split(' '); 
             numPalabras = palabras.Length;
 
-            // Si el telegrama es ordinario
+            
             if (tipoTelegrama == 'o')
             {
                 if (numPalabras <= 10)
@@ -53,29 +52,29 @@ namespace Telegrama
                 }
                 else
                 {
-                    // Se suma 2.5 por las primeras 10 palabras, y 0.5 por cada palabra adicional
+                    
                     coste = 2.5 + 0.5 * (numPalabras - 10);
                 }
             }
             else if (tipoTelegrama == 'u')
             {
-                // Telegrama urgente
+                
                 if (numPalabras > 10)
                 {
                     coste = 5 + 0.75 * (numPalabras - 10);
                 }
                 else
                 {
-                    // Se suma 5 por las primeras 10 palabras, y 0.75 por cada palabra adicional
+                    
                     coste = 5;
                 }
             }
             else
             {
-                coste = 0; // Esto se ejecuta si tipoTelegrama no es 'o' ni 'u'
+                coste = 0; 
             }
 
-            // Formateo del precio para mostrarlo con dos decimales
+
             txtPrecio.Text = coste.ToString("F2") + " euros";
         }
 
@@ -104,5 +103,6 @@ namespace Telegrama
         {
 
         }
+
     }
 }
